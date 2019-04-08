41ff6f874d75d9111281b5bb5e4968deedd7d211
Trevor Adams <trevor@trevoradams.dev>
2019-04-08 16:19:34 -0600

    updating about us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,13 +1,34 @@
 import React from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Svg from '../components/svg'
+
+const bulletStyles = css`
+  display: flex;
+  flex-direction: row;
+  margin: 0 0 10px 20px;
+
+  & svg:first-of-type {
+    margin-right: 12px;
+    min-width:24px;
+  }
+`
+
+const topSpace = css`
+  margin-top: 35px;
+`
+const orange = css`
+  color: #dc7f0d;
+`
 
 const AboutUs = () => (
   <Layout>
     <SEO title="About Us" />
-    <main className='textContainer'>
-    <h1>About Us</h1>
+    <main className="textContainer">
+      <h1>About Us</h1>
       <p>
         Following the economic crisis of 2008, there was significant
         consolidation in the banking sector. As banks began managing the assets
@@ -19,7 +40,7 @@ const AboutUs = () => (
         Unfortunately, this lack of awareness is resulting in millions of
         dollars of judgments expiring annually!
       </p>
-      <p>
+      <p css={topSpace}>
         In 2010, recognizing an opportunity to help banks and other financial
         institutions identify and manage their judgment portfolio, a team of
         attorneys, accountants and information professionals formed Guaranty
@@ -32,7 +53,27 @@ const AboutUs = () => (
         information our team of highly skilled debt counselors work with debtors
         to achieve a successful satisfaction of the judgment.
       </p>
-      <p>Whether a client chooses to:</p>
+      <p css={topSpace}>Whether a client chooses to:</p>
+      <div>
+        <div css={bulletStyles}>
+          <Svg icon="ChevronsRight" color="#DC7F0D" />
+          Have their judgment portfolio managed by Guaranty Solutions
+        </div>
+        <div css={bulletStyles}>
+          <Svg icon="ChevronsRight" color="#DC7F0D" />
+          License the Guaranty Solutions Judgment Management System to manage
+          their own portfolio
+        </div>
+        <div css={bulletStyles}>
+          <Svg icon="ChevronsRight" color="#DC7F0D" />
+          Sell their judgment portfolio to Guaranty Solutions at prevailing
+          market rates
+        </div>
+      </div>
+      <p style={{fontWeight: 500}} css={topSpace}>
+        It is easy to see that we truly …{' '}
+        <span css={orange}>Turn your write-offs into revenue!</span>
+      </p>
     </main>
   </Layout>
 )
