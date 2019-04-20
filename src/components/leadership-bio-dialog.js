00a2900c7676930a7102ffa968fff0a6d56c0b5e
Trevor Adams <trevor@trevoradams.dev>
2019-04-20 14:13:51 -0600

    dialog updates - doesn't include a lot of things still

---
+++ src/components/leadership-bio-dialog.js
@@ -0,0 +1,111 @@
+import React, { useEffect } from 'react' // eslint-disable-line no-unused-vars
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Drawer from '@material-ui/core/Drawer'
+import Svg from '../components/svg'
+
+import Bios from '../biographies/bios'
+// import Svg from './svg'
+
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+const closeBtn = css`
+  position: fixed;
+  margin-left: 20px;
+  background-color: #3194d2;
+  border-radius: 50%;
+  height: 64px;
+  width: 64px;
+  z-index: 10;
+
+  &:hover {
+    background-color: #2a81b8;
+  }
+`
+const bioOverlay = css`
+  position: absolute;
+  bottom: 0;
+  height: 125px;
+  width: 100%;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0.9) 49.83%,
+    rgba(255, 255, 255, 0) 148.4%
+  );
+  transform: rotate(-180deg);
+`
+const dialogTitle = css`
+  position: fixed !important;
+  background-color: #fff;
+  width: calc(100% - 432px);
+  max-width: 946px;
+  margin-top: -16px;
+  height: 120px;
+  font-family: 'Montserrat' sans-serif;
+  letter-spacing: 0.08em;
+
+  * {
+    color: #403a34;
+  }
+
+  .name {
+    font-size: 38px;
+    font-weight: bold;
+  }
+  .position {
+    font-size: 22px;
+  }
+`
+
+const LeadershipBioDialog = ({
+  config: { image, name, position, bioName = '', phone = '', fax = '', email },
+  dialogOpen,
+  dialogCloseHandler,
+}) => {
+  return (
+    <div className="bioDialogWrapper">
+      <Dialog
+        open={dialogOpen}
+        onClose={dialogCloseHandler}
+        onBackdropClick={dialogCloseHandler}
+        maxWidth="lg"
+        scroll="paper"
+        aria-labelledby="scroll-dialog-title"
+      >
+        <Drawer
+          className="bioSidebar"
+          variant="permanent"
+          open={true}
+          anchor="left"
+        >
+          <img src={image} alt={name} style={{ objectFit: 'contain' }} />
+        </Drawer>
+        <section className="bioSection">
+          <DialogActions>
+            <Button css={closeBtn} onClick={dialogCloseHandler}>
+              <Svg icon="CloseX" color="#f2f2f2" />
+            </Button>
+          </DialogActions>
+          <DialogTitle id="scroll-dialog-title" css={dialogTitle}>
+            <div className="name">{name}</div>
+            <div className="position">{position}</div>
+          </DialogTitle>
+          <DialogContent style={{ margin: '120px 0' }}>
+            {Bios[bioName].map((text, index) => (
+              <p key={`${name}${index}`} className="bioText">
+                {text}
+              </p>
+            ))}
+            <div css={bioOverlay} />
+          </DialogContent>
+        </section>
+      </Dialog>
+    </div>
+  )
+}
+
+export default LeadershipBioDialog
--- src/pages/management.js
+++ src/pages/management.js
@@ -2,14 +2,22 @@ import React from 'react' // eslint-disable-line no-unused-vars
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import LeadershipSpotlight from '../components/leadership-spotlight'
+import LeadershipBioDialog from '../components/leadership-bio-dialog'
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
+import bioList from '../biographies/bioConfig'
 import brian from '../images/Brian.jpg'
 import guy from '../images/Guy.jpg'
 import dustin from '../images/Dustin.jpg'
 import sam from '../images/Sam.png'
+import Svg from '../components/svg'
 
+const bioImages = {
+  brian,
+  guy,
+  dustin,
+  sam,
+}
 const title = css`
   text-align: center;
 `
@@ -36,72 +44,112 @@ const grid = css`
     justify-content: center;
   }
 `
+const bioButton = css`
+  position: relative;
+  margin-bottom: 40px;
+  font-size: 18px;
 
-const Management = () => (
-  <Layout>
-    <SEO title="Management" />
-    <main className="textContainer">
-      <h1 css={title}>Management</h1>
-      <p css={sub}>
-        The integrity of our processes reflects the professionalism and
-        standards of our team.
-      </p>
+  .imageWrapper {
+    height: 220px;
+    width: 220px;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-bottom: 10px;
 
-      <div css={grid}>
-        <LeadershipSpotlight
-          config={{
-            image: brian,
-            name: 'Brian Evans',
-            position: 'Managing Partner',
-            bioName: 'brian',
-            phone: '480.924.6499',
-            fax: '480.240.4187',
-            email: 'bevans@guaranty-solutions.com',
-          }}
-        />
-        <LeadershipSpotlight
-          config={{
-            image: guy,
-            name: 'Guy Adams',
-            position: 'Chief Information Officer',
-            bioName: 'guy',
-            phone: '480.924.6499',
-          }}
-        />
-        <LeadershipSpotlight
-          config={{
-            image: dustin,
-            name: 'Dustin Horne',
-            position: 'General Counsel',
-            bioName: 'dustin',
-            phone: '480.924.6499',
-          }}
-        />
-        <LeadershipSpotlight
-          config={{
-            image: sam,
-            name: 'Sam Evans',
-            position: 'Director of Research',
-            bioName: 'sam',
-            phone: '480.924.6499',
-          }}
-        />
-      </div>
+    img[data-bioname='sam'] {
+      margin-top: -15px;
+    }
+  }
+  .arrowFab {
+    position: absolute;
+    bottom: 70px;
+    left: 170px;
+    border-radius: 50%;
+    background-color: #fdfdfd;
+    height: 70px;
+    width: 70px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    svg {
+      position: relative;
+      top: 25px;
+      left: 22px;
+    }
+  }
+  .name {
+    font-weight: bold;
+  }
+`
 
-      <p css={desc}>
-        Combined, our team has more than 70 years of experience in the business
-        of finance and law. Our team comprises professionals from accounting,
-        law, private equity fund management, investments, and financial
-        operations.
-      </p>
-      <p css={desc}>
-        We understand your business and the business of your clients. Our
-        promise to you is a professional interaction of trust and intelligence
-        in finding solutions. Our clients find us forthright and capable at
-        deploying the appropriate customized solutions for your needs.
-      </p>
-    </main>
-  </Layout>
-)
+const Management = () => {
+  const [bioSelection, setBioSelection] = React.useState({})
+  const [dialogOpen, setDialogOpen] = React.useState(false)
+
+  const assignBio = bioName => {
+    let config = bioList.find(bio => bio.bioName === bioName)
+    if (config) {
+      config.image = bioImages[bioName]
+      console.log('config', config)
+      setBioSelection(config)
+      setDialogOpen(true)
+    }
+  }
+  const dialogCloseHandler = () => {
+    setDialogOpen(false)
+  }
+  return (
+    <Layout>
+      <SEO title="Management" />
+      <main className="textContainer">
+        <h1 css={title}>Management</h1>
+        <p css={sub}>
+          The integrity of our processes reflects the professionalism and
+          standards of our team.
+        </p>
+
+        <div css={grid}>
+          {bioList.map(({ name, position, bioName }) => (
+            <div css={bioButton} key={name}>
+              <div
+                className="imageWrapper"
+                role="button"
+                onClick={() => assignBio(bioName)}
+              >
+                <img
+                  src={bioImages[bioName]}
+                  alt={name}
+                  data-bioname={bioName}
+                />
+                <div className="arrowFab">
+                  <Svg icon="ArrowRight" color="#3194D2" />
+                </div>
+              </div>
+              <div className="name">{name}</div>
+              <div className="position">{position}</div>
+            </div>
+          ))}
+        </div>
+
+        <LeadershipBioDialog
+          config={bioSelection}
+          dialogOpen={dialogOpen}
+          dialogCloseHandler={dialogCloseHandler}
+        />
+
+        <p css={desc}>
+          Combined, our team has more than 70 years of experience in the
+          business of finance and law. Our team comprises professionals from
+          accounting, law, private equity fund management, investments, and
+          financial operations.
+        </p>
+        <p css={desc}>
+          We understand your business and the business of your clients. Our
+          promise to you is a professional interaction of trust and intelligence
+          in finding solutions. Our clients find us forthright and capable at
+          deploying the appropriate customized solutions for your needs.
+        </p>
+      </main>
+    </Layout>
+  )
+}
 
 export default Management
